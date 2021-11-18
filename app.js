73f4dee4dfe789d5f414f542385f9361b10fc713
Hemu GX <gxhemu@gmail.com>
2021-11-18 09:01:51 +0600

    Level 4 - bcrypt and Salting

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-//Enviromental Variables
+//Encrypting Password with bcrypt Hash and salting.
 
-require("dotenv").config()  //Require it as early as possible and perform config method.
+require("dotenv").config() //Require it as early as possible and perform config method.
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")  //Require MD5
+const bcrypt = require("bcrypt"); //Require bcrypt
+const saltRounds = 10; //This specify how many rounds it will salt through.
+
 
 
 
@@ -22,7 +24,7 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/user1DB")
 
-const userSchema = new mongoose.Schema ({ //Must use the complete mongoose schema form
+const userSchema = new mongoose.Schema({ //Must use the complete mongoose schema form
   email: String,
   password: String
 })
@@ -30,43 +32,61 @@ const userSchema = new mongoose.Schema ({ //Must use the complete mongoose schem
 
 
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.get("/home",function(req,res) {
+app.get("/home", function(req, res) {
   res.render("home")
 })
 
-app.get("/login",function(req,res) {
+app.get("/login", function(req, res) {
   res.render("login")
 })
 
-app.get("/register",function(req,res) {
+app.get("/register", function(req, res) {
   res.render("register")
 })
 
-app.post("/register",function(req,res) {
-  const newUser = new User ({
-    email: req.body.username,
-    password: md5(req.body.password),  //Converting Password into Hash
-  })
+app.post("/register", function(req, res) {
 
-  newUser.save(function(err){  //This will save user information to Dataase.
-    if(!err) {
-      res.render("secrets")   //If the registration was successfull ,the secret page will render,There is no oher way to render it cauz there is no get requesrt.
-    }
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) { //This wil auto gen salt.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash //Using the converted Hash password.
+    })
+
+    console.log(hash);
+
+    newUser.save(function(err) { //This will save user information to Dataase.
+      if (!err) {
+        res.render("secrets") //If the registration was successfull ,the secret page will render,There is no oher way to render it cauz there is no get requesrt.
+      }
+    })
+  });
+
+
 });
 
-app.post("/login",function(req,res) {
+app.post("/login", function(req, res) {
   userEmail = req.body.username;
-  userPassword = md5(req.body.password); //Converting login input password into hash to check if it matches with the hash in our database.
+  userPassword = req.body.password
+  User.findOne({
+    email: userEmail
+  }, function(err, foundUser) {
+    if (!err) {
+      if (foundUser) { //This will check if the user was found.
+        bcrypt.compare(userPassword, foundUser.password).then(function(result) { //This compares input password with password in Database.
+          if (result === true) { //If the result is true,the function will continue.
+            res.render("secrets")
+          }
+        });
 
-  User.findOne({email: userEmail},function(err,foundUser) {
-    if(!err) {
-      if(foundUser){  //This will check if the user was found.
-        if(foundUser.password === userPassword ){  //This will check if the password matched.
-          res.render("secrets")
-        }
+        bcrypt.compare(userPassword, foundUser.password).then(function(result) {
+          if (result === false) { //If the result was false ,this function will continue.
+            console.log("Wrong Password!");
+          }
+
+        });
+
       }
     }
   });
